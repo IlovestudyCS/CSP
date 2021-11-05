cfb0d861c8ee9d5315423989f2b9ff459412812d
lmn <3398467257@qq.com>
2021-11-05 21:16:12 +0800

    15 score

--- 1016PhoneBills.cpp
+++ 1016PhoneBills.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<algorithm>
 #include<vector>
+#include<iomanip>
 
 using namespace std;
 
@@ -34,6 +35,7 @@ class customer{
     int type;
     string time[4];
 };
+int calcu_cost(string time1[], int total_time);
 
 int main()
 {
@@ -68,20 +70,43 @@ int main()
         else if(c1.time[2] != c2.time[2]) return c1.time[2] < c2.time[2];
         else return c1.time[3] < c1.time[3];
     });
+    int total_cost = 0;
+    string prename;
     for(int i = 0; i < N - 1; i++){
-        if(v[i].name == v[i = 1].name && v[i].type == on_line && v[i + 1].type == off_line) {
+        if(v[i].name != prename) {
+            cout << v[i].name << " " << v[i].time[0] << endl;
+            prename = v[i].name;
+        }
+        if(v[i].name == v[i + 1].name && v[i].type == on_line && v[i + 1].type == off_line) {
             int total_time = 0, cost = 0;
             int day = atoi(v[i + 1].time[1].c_str()) - atoi(v[i].time[1].c_str());
             int hour = atoi(v[i + 1].time[2].c_str()) - atoi(v[i].time[2].c_str());
             int minute = atoi(v[i + 1].time[3].c_str()) - atoi(v[i].time[3].c_str());
 
             total_time = day * 24 * 60 + hour * 60 + minute;
-            
-            cout << v[i].name << " " << v[i].time[0] << endl;
+            cost = calcu_cost(v[i].time, total_time);
+            total_cost += cost;
             cout << v[i].time[1] <<":" << v[i].time[2] << ":" << v[i].time[3] <<
             " " << v[i + 1].time[1] <<":" << v[i + 1].time[2] << ":" << v[i + 1].time[3] <<
-            " " << total_time;
+            " " << total_time << " $" << fixed << setprecision(2) <<(double) cost / 100 << endl;
+        }
+        if(v[i].name != v[i + 1].name) {
+            cout << "Total amount: $" << fixed << setprecision(2) <<(double)total_cost / 100 << endl;
+            total_cost = 0;
         }
     }
+    cout << "Total amount: $" << fixed << setprecision(2) <<(double)total_cost / 100;
     return 0;
+}
+
+int calcu_cost(string time1[], int total_time)
+{
+    int cost = 0;
+    int day = atoi(time1[1].c_str());
+    int hour = atoi(time1[2].c_str());
+    int minute = atoi(time1[3].c_str());
+    for(int i = 1; i <= total_time; i++) {
+        cost += price[(hour + (i + minute - 1) / 60) % 24];
+    }
+    return cost;
 }
